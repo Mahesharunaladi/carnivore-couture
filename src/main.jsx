0b4e20f1f8bcc2f9925c1dce73c5f07f33831248
Mahesharunaladi <mahesharunaladi@gmail.com>
2025-10-19 11:28:43 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext'; // Add CartProvider import
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <AuthProvider> {/* Wrap App with AuthProvider */}
-      <App />
+    <AuthProvider>
+      <CartProvider> {/* Wrap App with CartProvider */}
+        <App />
+      </CartProvider>
     </AuthProvider>
   </BrowserRouter>
 );
