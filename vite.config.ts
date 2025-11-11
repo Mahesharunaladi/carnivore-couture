94f83d5d9adcc74bc73b2256f38e0eea3bf68af6
Mahesharunaladi <mahesharunaladi@gmail.com>
2025-11-11 21:50:56 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
+// ...
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
       // Add this line to match tsconfig.json
       '@/*': '/src/*',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
     },
   },
   server: {
